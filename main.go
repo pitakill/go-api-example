04efec570a2d8e999e3e1a6c59bbc336e998ee88
Lepoldo Caballero <git@pitakill.net>
2016-11-29 21:58:42 -0600

    Rename/Erase this commit

--- main.go
+++ main.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"net/http"
 
 	"gopkg.in/gin-gonic/gin.v1"
 )
+
+type Routes struct {
+	Routes []Route
+}
+
+type Route struct {
+	Name Name
+}
+
+type Name struct {
+	Code    int
+	Message string
+}
+
+func getMessagesRoutes() {
+	file, _ := os.Open("message.json")
+	decoder := json.NewDecoder(file)
+
+	var route Route
+
+	err := decoder.Decode(&route)
+
+	if err != nil {
+		fmt.Println("error", err)
+	}
+
+	fmt.Println(route)
+}
 
 func basicGetRoute(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
@@ -19,6 +51,8 @@ func notFoundRoute(c *gin.Context) {
 }
 
 func main() {
+	getMessagesRoutes()
+
 	r := gin.Default()
 
 	v1 := r.Group("api/v1")
